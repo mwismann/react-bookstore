78fcec006bb42986e0fb6abf427efe5ca33fde66
Matt <mwbertolucci@gmail.com>
2023-03-30 17:52:28 -0400

    Display status check msg on btn click of /categories

--- src/routes/Categories.js
+++ src/routes/Categories.js
@@ -1,3 +1,19 @@
-const Categories = () => <button type="button">Check status</button>;
+import { useState } from 'react';
+
+const Categories = () => {
+  const [checking, setChecking] = useState(false);
+
+  const handleClick = () => {
+    setChecking(true);
+  };
+
+  return (
+    <>
+      <button type="button" onClick={handleClick}>Check status</button>
+      {' '}
+      {checking && (<span>Checking status</span>)}
+    </>
+  );
+};
 
 export default Categories;
