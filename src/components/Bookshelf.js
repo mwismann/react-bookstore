7de41ba950f38474f4442d748e8541dcee300c4f
Matt <mwbertolucci@gmail.com>
2023-04-04 14:15:35 -0400

    Update initState and display books from slice

--- src/components/Bookshelf.js
+++ src/components/Bookshelf.js
@@ -1,21 +1,21 @@
-import PropTypes from 'prop-types';
-import BookItem from './BookItem';
+import { useSelector } from 'react-redux';
+import Book from './Book';
+import InputBook from './InputBook';
 
-const Bookshelf = ({ bookList, removeBook }) => (
-  <ul>
-    {bookList.map((book) => (
-      <BookItem key={book.id} item={book} removeBook={removeBook} />
-    ))}
-  </ul>
-);
-
-Bookshelf.propTypes = {
-  bookList: PropTypes.arrayOf(PropTypes.shape({
-    id: PropTypes.number,
-    title: PropTypes.string,
-    author: PropTypes.string,
-  })).isRequired,
-  removeBook: PropTypes.func.isRequired,
+const Bookshelf = () => {
+  const { bookList } = useSelector((state) => state.bookList);
+  return (
+    <>
+      <section className="bookshelf">
+        <ul>
+          {bookList.map((book) => (
+            <Book key={book.id} id={book.id} title={book.title} author={book.author} />
+          ))}
+        </ul>
+      </section>
+      <InputBook />
+    </>
+  );
 };
 
 export default Bookshelf;
--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,25 +1,25 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  value: [
+  bookList: [
     {
-      "item_id": "item1",
-      "title": "The Great Gatsby",
-      "author": "John Smith",
-      "category": "Fiction"
+      id: 'item1',
+      title: 'The Great Gatsby',
+      author: 'John Smith',
+      category: 'Fiction',
     },
     {
-      "item_id": "item2",
-      "title": "Anna Karenina",
-      "author": "Leo Tolstoy",
-      "category": "Fiction"
+      id: 'item2',
+      title: 'Anna Karenina',
+      author: 'Leo Tolstoy',
+      category: 'Fiction',
     },
     {
-      "item_id": "item3",
-      "title": "The Selfish Gene",
-      "author": "Richard Dawkins",
-      "category": "Nonfiction"
-    }
+      id: 'item3',
+      title: 'The Selfish Gene',
+      author: 'Richard Dawkins',
+      category: 'Nonfiction',
+    },
   ],
 };
 
