4c1d54361aec5f1e1dc60583408bc848800ce616
Matt <mwbertolucci@gmail.com>
2023-04-07 13:14:38 -0400

    Style Categories route component

--- src/components/Bookshelf.js
+++ src/components/Bookshelf.js
@@ -24,7 +24,12 @@ const Bookshelf = () => {
   return (
     <div className="container w-10/12 mx-auto">
       <section className="py-9 border-b-2 border-[#e8e8e8]">
-        {isLoading && <h2 className="text-xl text-[#0290ff]">Loading...</h2>}
+        {isLoading && (
+          <div className="container my-8 mx-auto flex items-center justify-center gap-3">
+            <div className="w-4 h-4 border-2 border-[#379cf6] border-l-[#e8e8e8] rounded-full animate-spin" />
+            <h2 className="text-2xl font-semibold text-[#0290ff]">Loading</h2>
+          </div>
+        )}
         {error && <h2 className="text-xl text-[#dc2929]">{error}</h2>}
         <ul className="flex flex-col gap-5">
           {books}
