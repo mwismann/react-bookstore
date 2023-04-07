c5183a7f9fe40b5e110e796cd06ae2939c0eb0ac
Matt <mwbertolucci@gmail.com>
2023-04-07 12:45:00 -0400

    Style InputBook component

--- src/components/InputBook.js
+++ src/components/InputBook.js
@@ -35,10 +35,11 @@ const InputBook = () => {
   };
 
   return (
-    <form onSubmit={(e) => handleSubmit(e)}>
-      <input type="text" placeholder="Add title" value={title} onChange={handleTitleChange} required />
-      <input type="text" placeholder="Add author" value={author} onChange={handleAuthorChange} required />
-      <button type="submit">Submit</button>
+    <form className="mt-6" onSubmit={(e) => handleSubmit(e)}>
+      <h2 className="mb-4 text-xl text-[#888] font-bold uppercase tracking-tight">Add New Book</h2>
+      <input type="text" className="w-2/5 py-2 px-4 text-[#121212] tracking-tighter border border-[#e8e8e8] rounded placeholder-[#c4c4c4] focus:outline-[#0290ff]" placeholder="Add title" value={title} onChange={handleTitleChange} required />
+      <input type="text" className="w-2/5 mx-8 py-2 px-4 text-[#121212] tracking-tighter border border-[#e8e8e8] rounded placeholder-[#c4c4c4] focus:outline-[#0290ff]" placeholder="Add author" value={author} onChange={handleAuthorChange} required />
+      <button type="submit" className="py-2.5 px-14 uppercase tracking-wide font-['Roboto_Slab'] font-bold text-sm text-white bg-[#0290ff] rounded">Add book</button>
     </form>
   );
 };
