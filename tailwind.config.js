6bb7cb7de6ad613cbe7899cf331277412b76e3ef
Matt <mwbertolucci@gmail.com>
2023-04-07 13:14:11 -0400

    Install tailwind animation delay

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const req = require('tailwindcss-animation-delay');
+
 module.exports = {
   content: [
     './src/**/*.{js,jsx,ts,tsx}',
@@ -6,5 +8,7 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    req,
+  ],
 };
